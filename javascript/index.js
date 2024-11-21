ed2e070c38392be8cd395f80ebc114a3bc33a5f4
summersideup <sadesommers@gmail.com>
2024-11-20 19:22:31 -0500

    Updated CSS

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,5 @@
 function updateTime() {
+  // Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
   if (losAngelesElement) {
     let losAngelesDateElement = losAngelesElement.querySelector(".date");
@@ -11,6 +12,7 @@ function updateTime() {
     );
   }
 
+  // Paris
   let parisElement = document.querySelector("#paris");
   if (parisElement) {
     let parisDateElement = parisElement.querySelector(".date");
@@ -22,15 +24,31 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+
+  //  Beijing
+  let beijingElement = document.querySelector("#beijing");
+  if (beijingElement) {
+    let beijingDateElement = beijingElement.querySelector(".date");
+    let beijingTimeElement = beijingElement.querySelector(".time");
+    let beijingTime = moment().tz("Asia/Shanghai");
+
+    beijingDateElement.innerHTML = beijingTime.format("MMMM Do YYYY");
+    beijingTimeElement.innerHTML = beijingTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
+
   let citiesElement = document.querySelector("#cities");
 
   citiesElement.innerHTML = `
@@ -44,6 +62,15 @@ function updateCity(event) {
   )}</small></div>
     </div>
   `;
+
+  let allCitiesLinkContainer = document.querySelector(
+    "#all-cities-link-container"
+  );
+  allCitiesLinkContainer.innerHTML = `<a href="https://globalmeridian.netlify.app" 
+        style="display: block; text-align: center; margin-top: 20px; 
+               color: #4A90E2; text-decoration: none;">
+        All Cities
+    </a>`;
 }
 
 updateTime();
